532de4a9182a3d2b43b8e5c8f591fa879e451a1e
Rene1814 <rne_souza@yahoo.com.br>
2023-07-04 06:29:22 -0300

    Server layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
-@RestController //essa anotação diz que essa classe é um recurso web que é implementado por um controlador rest
-@RequestMapping(value = "/users") //essa anotação é o nome do recurso
+@RestController // essa anotação diz que essa classe é um recurso web que é implementado por um
+				// controlador rest
+@RequestMapping(value = "/users") //o q vai entre aspas é usado no navegador com localhost:8080 para o postman
 public class UserResource {
-	
-	//método que é um endpoint pra acessar os usuários
+
+	@Autowired
+	private UserService service;
+
+	// método que é um endpoint pra acessar os usuários
 	// o tipo de retorno ResponseEntity é pra retornar respostas de requisições web
 	@GetMapping  //ess anotação indica que esse método abaixo responde à requisição tipo get do http
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u); //.ok() retorna resposta com sucesso do http, .body(u) retorna no corpo da resposta o usuário u q acabei de instanciar
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); //.ok() retorna resposta com sucesso do http, .body(u) retorna no corpo da resposta o usuário u q acabei de instanciar
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
